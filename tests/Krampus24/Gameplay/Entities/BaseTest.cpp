1e5e13a303e035101f9a5f568e5bc5d35945edc1
Mark Oates <markoates0@gmail.com>
2024-12-01 14:48:51 -0500

    * Add (somewhat overcomplex) Entities/Base

---
+++ tests/Krampus24/Gameplay/Entities/BaseTest.cpp
@@ -0,0 +1,12 @@
+
+#include <gtest/gtest.h>
+
+#include <Krampus24/Gameplay/Entities/Base.hpp>
+
+
+TEST(Krampus24_Gameplay_Entities_BaseTest, can_be_created_without_blowing_up)
+{
+   Krampus24::Gameplay::Entities::Base base;
+}
+
+
